8d483223821a78f23884a88f21bf79d161f6cc19
root <root@PiCam.fritz.box>
2014-01-22 15:58:55 +0000

    Script weiter ausgebaut

--- timelapse.sparky0815.sh
+++ timelapse.sparky0815.sh
@@ -1,25 +1,48 @@
 #!/bin/sh
 
 #################################
-## Timelapse by Sparky0815.de
-## Version: 0.2
-## Date: 2014-01-21
-##
+## Timelapse by Sparky0815.de  ##
+## Version: 0.3		       ##
+## Date: 2014-01-22	       ##
+##			       ##
+################################################################################################################################
+## Einstellungen:
 
-## possible resolutions
-
-## Settings
-
+## Speicherort für die Bilder
 path="/home/pi/timelapse_pics"
-filename="Timelapse-201401"
+
+## Dateiname
+filename="Timelapse"
+
+## Höhe (max. 2593)
 height="2592"
+
+## Breite des Bildes (max. 1944)
 width="1944"
-quality="75" 
+
+## Qualität (0-100)
+quality="100"
+
+## Exposure: off,auto,night,nightpreview,backlight,spotlight,sports,snow,beach,verylong,fixedfps,antishake,fireworks
+exposure="auto"
+
+## AWB: off,auto,sun,cloud,shade,tungsten,fluorescent,incandescent,flash,horizon
+awb="auto"
+
+## Anzahl der Bilder
 picture_amount="5"
+
+## Wartezeit zwischen der Aufnahme (in ms bsp.: 15000 = 15 sekunden)
 wait_time="2000"
 
+## Bild drehen (0-359)
+rotate="0"
 
-## Show settings on console
+################################################################################################################################
+
+
+
+## Einstellungen anzeigen
 
 show_setup() {
 clear
@@ -30,8 +53,11 @@ echo "Dateiname: "$filename
 echo "Hoehe: "$height
 echo "Breite: "$width
 echo "Qualitaet: "$quality 
-echo "Wartezeit:"$wait_time
-echo "Anzahl Bilder:" $picture_amount
+echo "Exposure "$exposure
+echo "Automaticher Weiss Abgleich (AWB): "$awb
+echo "Bild drehen um (grad): "$rotate
+echo "Wartezeit: "$wait_time
+echo "Anzahl Bilder: "$picture_amount
 echo
 echo "Soll mit diesen Einstellungen gestartet werden ? (j/n)"
 echo 
@@ -39,7 +65,13 @@ echo
 
 
 
-## Start timelapse
+## Speicherort für Bilder leeren bevor die Aufnahme startet
+
+sudo rm -rf $path"/*.jpg"
+
+
+
+## Aufnamhe ausfuehren
 
 start_question() {
 read answer
@@ -61,22 +93,25 @@ echo "Abgebrochen. Bitte ggf. Einstellungen in der timelapse.sparky0815.sh vorne
 esac
 }
 
-## Take pictures
+
+
+## Bilder aufnehmen
 
 take_picture() {
 number="0"
+date=`date +%F`
 while [ $number -lt $picture_amount ]
 do 
 rest=`expr $picture_amount - $number`
 echo "Es muessen noch " $rest " Bilder gemacht werden..."
-raspistill -o $path"/"$filename"_"$number".jpg" -h $height -w $width -q $quality -t $wait_time
+raspistill -o $path"/"$filename"-"$date"_"$number".jpg" -h $height -w $width -q $quality -t $wait_time -ex $exposure -awb $awb -rot $rotate
 number=`expr $number + 1`
 done
 }
 
 
 
-## Finish message
+## Ende Nachricht
 
 show_finished() {
 echo
@@ -84,6 +119,9 @@ echo "Fertig."
 echo "Es wurden" $number "Bilder im Ordner" $path "abgelegt."
 }
 
+
+
+## Einstellungen anzeigen und fragen ob mit diesen die Aufnahme gestartet werden soll
 
 show_setup
 start_question
